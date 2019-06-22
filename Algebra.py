dfd410e11a93e38479c0fa8e1c597bf8177fb886
k6b2yjxma7li <rkrasala@gmail.com>
2019-06-22 11:02:56 +0200

    Yet another try to define operators

--- Algebra.py
+++ Algebra.py
@@ -17,17 +17,7 @@ Constants:
 ---
 """
 
-import cmath
 
 class Operator:
-    def __init__(self, operation, arg1, arg2=None):
-        self.operation = operation
-        self.arg1 = arg1
-        if arg2 is not None:
-            self.arg2 = arg2
-
-    def __str__(self):
-        if self.arg2 is not None:
-            return f"{self.arg1} {self.operation} {self.arg2}"
-        else:
-            return f"{self.operation} {self.arg1}"
+    def __init__(self):
+        pass
