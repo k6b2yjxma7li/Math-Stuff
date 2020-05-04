69625cb3a0731eb7c6f43126ccba85e195c662dd
k6b2yjxma7li <rasalrob@orange.com>
2020-05-04 22:34:17 +0200

    Return of proper settings for hbn

--- worksite.py
+++ worksite.py
@@ -62,17 +62,20 @@ raman = spectrum(lorentz, 3)
 dif = residual(raman, x_av, y_av)
 ressq = residual(raman, x_av, y_av, 1/y_av**0.5)
 res = residual(raman, x_av, y_av, y_stdev)
-M = 100
-scale = 1
+M = 50
+# scale = 1
 
 y = y_av
-curve_count = 20
+curve_count = 15
 initial = True
 solutions = []
 
+#
+r = dif([0, 1, 1])
+
 # %%
 # Main resgd fitter
-r = dif([0, 1, 1])
+
 while len(sol)/3 < curve_count:
     gd = gdev(r, M)
     resgd = residual(raman, x_av, y, 1/gd)
@@ -83,8 +86,8 @@ while len(sol)/3 < curve_count:
     v = [abs(Amp), hmhw, x0]
     sol = list(sol)
     sol += [Amp, hmhw, x0]
-    if len(sol)/3 % 3 == 0 or curve_count - len(sol)/3 < 3:
-        sol, h = leastsq(resgd, sol)
+    # if len(sol)/3 % 3 == 0 or curve_count - len(sol)/3 < 3:
+    sol, h = leastsq(resgd, sol)
     r = dif(sol)
     # print(f"{int(len(sol)/3)}: {r.dot(r)}")
     print(f"{int(len(sol)/3)}: {percentage(r.dot(r), y.dot(y))}")
@@ -132,7 +135,7 @@ while True:
             return np.array(data)
         return DENS
 
-    sgm = 0.5
+    sgm = 0.1
     density = set_density(surface, active, sgm, sgm)
 
     def density_estimator(u):
@@ -143,7 +146,7 @@ while True:
     est = np.mean
 
     X, Y = np.meshgrid(np.linspace(min(surface)-1, max(surface)+1, 1000),
-                       np.linspace(min(active)-1, max(active)+1, 1000))
+                    np.linspace(min(active)-1, max(active)+1, 1000))
 
     R = pearson(surface, active, estimator=est)
     s_std = (est(surface**2) - est(surface)**2)**0.5
@@ -187,12 +190,12 @@ while True:
 #     sola = sol
 #     curve_count = int(len(sol)/3)
 #     initial = False
-sola = sol
+# sola = sol
 
 # %%
 # Main fitter plotter
 K = M
-res = residual(raman, x_av, y, y_stdev)
+# res = residual(raman, x_av, y, y_stdev)
 r = dif(sol)
 fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(10, 10))
 ax[0].set_title(f"Average data with stddev")
@@ -274,21 +277,21 @@ for dset in tbl.keys():
         ## %%
         # Active vs overall surface
     
-        fig, ax = plt.subplots()
-        fig.set_size_inches(15, 15)
-        ax.set_title(f"Curve selector for {dset} degrees {direct}")
-        ax.set_xlabel('Surface (log10)')
-        ax.set_ylabel('Active surface (log10)')
+        # fig, ax = plt.subplots()
+        # fig.set_size_inches(15, 15)
+        # ax.set_title(f"Curve selector for {dset} degrees {direct}")
+        # ax.set_xlabel('Surface (log10)')
+        # ax.set_ylabel('Active surface (log10)')
         surface = []
         active = []
         for n in range(0, len(sol)-1, 3):
             surface += [np.log10(abs(sol[n]*sol[n+1]))]
             active += [np.log10(abs(sum(lorentz(sol[n:n+3], x_av)*d(x_av))))]
-            ax.text(surface[-1], active[-1], f"{int(n/3)}", ha='left')
+            # ax.text(surface[-1], active[-1], f"{int(n/3)}", ha='left')
         surface = np.array(surface)
         active = np.array(active)
-        ax.plot(surface, active, '.', color=glob_style([0,0,0]))
-        ax.set_aspect(aspect='equal', adjustable='box')
+        # ax.plot(surface, active, '.', color=glob_style([0,0,0]))
+        # ax.set_aspect(aspect='equal', adjustable='box')
 
         def bivariate(u, v, r):
             return np.exp(-(u**2 - 2*r*u*v + v**2)/(2*(1-r**2)**0.5))
@@ -310,7 +313,7 @@ for dset in tbl.keys():
                 return np.array(data)
             return DENS
 
-        sgm = 0.5
+        sgm = 0.1
         density = set_density(surface, active, sgm, sgm)
 
         def density_estimator(u):
@@ -334,15 +337,15 @@ for dset in tbl.keys():
 
         requirement = bivariate((surface-s_m)/s_std, (active-a_m)/a_std, R) >= 0.05
 
-        for n in range(len(surface)):
-            if not requirement[n]:
-                ax.plot(surface[n], active[n], '.', color='red')
+        # for n in range(len(surface)):
+        #     if not requirement[n]:
+        #         ax.plot(surface[n], active[n], '.', color='red')
 
-        ax.plot(s_m, a_m, '+', ms=5, color=[1,0,1])
-        ax.text(s_m, a_m, f"({round(s_m, 2)},{round(a_m, 2)})", color=[1,1,0], fontsize=8)
-        levs = np.array([1e-4, 2e-4, 5e-4, 0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1])*max(Z.flatten())
-        cs = ax.contour(X, Y, Z, linewidths=0.7, levels=levs)
-        ax.clabel(cs, inline=True, fontsize=8)
+        # ax.plot(s_m, a_m, '+', ms=5, color=[1,0,1])
+        # ax.text(s_m, a_m, f"({round(s_m, 2)},{round(a_m, 2)})", color=[1,1,0], fontsize=8)
+        # levs = np.array([1e-4, 2e-4, 5e-4, 0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1])*max(Z.flatten())
+        # cs = ax.contour(X, Y, Z, linewidths=0.7, levels=levs)
+        # ax.clabel(cs, inline=True, fontsize=8)
 
 
         ## %%
@@ -356,7 +359,7 @@ for dset in tbl.keys():
                     sol.pop(n)
         # fig.savefig(f"./.data/{direct}/selector/{dset}_selector_{direct}_{ix}.pdf")
         ix += 1
-        if False not in requirement:
+        if False not in requirement and r.dot(r)/(y.dot(y)) < 0.01 :
             print("No trimming")
             break
     # setting number of curves 
@@ -416,56 +419,6 @@ for dset in tbl.keys():
                 ax.plot(sol[n:n+3][2], abs(sol[n:n+3][0]), '.', ms=2, color=glob_style([1,0,0]))
                 ax.text(sol[n:n+3][2], 14000, f"[{int(n/3)}]  {str(round(abs(sol[n:n+3][0]),1))}")
     # fig.savefig(f"./.data/{direct}/comp/{dset}_{direct}_comp.pdf")
-
-
-
-# %%
-dset = "000"
-y = tbl[dset]["#Intensity"]
-sol = solutions[0]
-res = residual(raman, x_av, y, y_stdev)
-dif = residual(raman, x_av, y)
-r = dif(sol)
-fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(10, 10))
-ax[0].set_title(f"Data with stddev for {dset} degrees {direct}")
-ax[1].set_title("Residual with average and deviations")
-ax[0].plot(x_av, y, '.', ms=0.7, lw=0.7, color=glob_style([0,0,0]))
-ax[0].plot(x_av, y_stdev, '.', ms=0.7, lw=0.7, color=glob_style([0,0,1]))
-
-ax1 = plt.twinx(ax[0])
-ax1.set_ylim([0, 1200])
-
-ax[1].plot(x_av, r, '.', lw=0.7, ms=0.8, color=glob_style([0,0,0]))
-
-ax2 = plt.twinx(ax[1])
-ax2.set_ylim([0, 1200])
-ax2.plot(x_av, gdev(r, K), '-', lw=0.7, ms=0.7, color=glob_style([0,0,0]))
-ax2.plot(x_av, np.linspace(np.std(r), np.std(r), len(x_av)), '--', lw=0.7, ms=0.7, color=glob_style([0,0,1]))
-
-ax[1].plot(x_av, np.linspace(np.mean(r), np.mean(r), len(x_av)), '-.', lw=0.7, ms=0.7, color=glob_style([0,0,1]))
-ax[1].plot(x_av, smoothing(r, K), '--', lw=0.7, ms=0.7, color=glob_style([0,0,0]))
-
-ax1.plot(x_av, gdev(r, K), '-', lw=0.7, ms=0.7, color=glob_style([0,0,0]))
-
-ax[0].plot(x_av, lorentz(v, x_av), '-', color=glob_style([0,0,0,0.3]), lw=0.9, ms=0.9)
-ax[0].plot(x_av, raman(sol, x_av), '-', color=glob_style([1,0,0]), lw=0.9)
-ax[0].plot(x_av, raman(sol, x_av)+smoothing(r, K), '--', color=glob_style([1,0,0]), lw=0.9)
-
-fig, ax = plt.subplots(figsize=(10, 10))
-ax.set_title(f"Spectral components for average model")
-ax.set_xlim([min(x_av), max(x_av)])
-ax.set_ylim([0, max(y)*1.25])
-ax.plot(x_av, y, '.', ms=2, color=glob_style([0,0,0]))
-ax.plot(x_av, raman(sol, x_av), '-', lw=0.7, color=glob_style([1,0,0]))
-for n in range(0, len(sol)-1, 3):
-    ax.plot(x_av, lorentz(sol[n:n+3], x_av), lw=0.7, color=glob_style([0,0,0,0.3]))
-    if 500 < sol[n:n+3][2] < 3000:
-        if 0 < abs(sol[n:n+3][0]) < 14000:
-            ax.plot(sol[n:n+3][2], abs(sol[n:n+3][0]), '.', ms=2, color=glob_style([1,0,0]))
-            ax.text(sol[n:n+3][2], abs(sol[n:n+3][0]), f"[{int(n/3)}]  {str(round(abs(sol[n:n+3][0]),1))}")
-        else:
-            ax.plot(sol[n:n+3][2], abs(sol[n:n+3][0]), '.', ms=2, color=glob_style([1,0,0]))
-            ax.text(sol[n:n+3][2], 14000, f"[{int(n/3)}]  {str(round(abs(sol[n:n+3][0]),1))}")
 # %%
 # Radial for chosen peak
 # fig, ax = plt.subplots(figsize=(10, 10))
