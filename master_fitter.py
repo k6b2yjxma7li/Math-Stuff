a76f688827d4a77a1225dc8c4797ccb04327aa7d
k6b2yjxma7li <rkrasala@gmail.com>
2020-07-16 19:59:52 +0200

    Formatting: fixing issues raised by incompatibility with PEP8

--- master_fitter.py
+++ master_fitter.py
@@ -18,9 +18,12 @@ from nano.table import table
 from nano.addfun import gdev, lorentz, residual, spectrum
 
 warnings.filterwarnings("ignore")
-
-glob_style = lambda clr: np.array([1,1,1,1])[:len(clr)]-np.array(clr)
 plt.style.use('dark_background')
+gav = smoothing
+
+
+def glob_style(clr): return np.array([1, 1, 1, 1])[:len(clr)]-np.array(clr)
+
 
 def percentage(u, v):
     val = 100*u/v
@@ -28,7 +31,6 @@ def percentage(u, v):
     # print(dig)
     return f"{round(val, dig)}%"
 
-gav = smoothing
 
 # %%
 # Configuration
@@ -122,13 +124,19 @@ sol = []
 y = y_av
 initial = True
 
-
 r = dif([0, 1, 1])
+
+
+def resg_gen(fn, u, v, w):
+    def _res_(slt):
+        return residual(fn, u, v, w)(slt)*np.array(slt).dot(np.array(slt))
+    return _res_
+
 
 M = config[material]['M']['init']
 while len(sol)/3 < config[material]['init']:
     gd = (gdev(r, M)**2 + gav(r, M)**2)  # V2
-    # resgd = lambda s: residual(raman, x_av, y, 1/gd)(s)*np.array(s).dot(np.array(s))
+    # resgd = resg_gen(raman, x_av, y, 1/gd)
     resgd = residual(raman, x_av, y, 1/gd)
     p = xpeak(x_av, gd, max(gd), max(gd)/2)
     hmhw = abs(x_av[p[0]] - x_av[p[-1]])/2
@@ -146,7 +154,7 @@ while len(sol)/3 < config[material]['init']:
 M = config[material]['M']['final']
 while len(sol)/3 < config[material]['count']:
     gd = (gdev(r, M)**2 + gav(r, M)**2)  # V2
-    # resgd = lambda s: residual(raman, x_av, y, 1/gd)(s)*np.array(s).dot(np.array(s))
+    # resgd = resg_gen(raman, x_av, y, 1/gd)
     resgd = residual(raman, x_av, y, 1/gd)
     p = xpeak(x_av, gd, max(gd), max(gd)/2)
     hmhw = abs(x_av[p[0]] - x_av[p[-1]])/2
@@ -180,7 +188,7 @@ while True:
         ax.text(surface[-1], active[-1], f"{int(n/3)}", ha='left')
     surface = np.array(surface)
     active = np.array(active)
-    ax.plot(surface, active, '.', color=glob_style([0,0,0]))
+    ax.plot(surface, active, '.', color=glob_style([0, 0, 0]))
     ax.set_aspect(aspect='equal', adjustable='box')
 
     def bivariate(u, v, r):
@@ -195,11 +203,12 @@ while True:
             x_std = np.linspace(x_std, x_std, len(x_p))
         if '__len__' not in dir(y_std):
             y_std = np.linspace(y_std, y_std, len(y_p))
+
         def DENS(u, v):
             data = 0
             for n in range(len(x_p)):
-                data += single_density((u-x_p[n])/x_std[n], 
-                                    (v-y_p[n])/y_std[n])/(x_std[n]*y_std[n])
+                data += single_density((u-x_p[n])/x_std[n],
+                                       (v-y_p[n])/y_std[n])/(x_std[n]*y_std[n])
             return np.array(data)
         return DENS
 
@@ -214,7 +223,7 @@ while True:
     est = np.mean
 
     X, Y = np.meshgrid(np.linspace(min(surface)-1, max(surface)+1, 1000),
-                    np.linspace(min(active)-1, max(active)+1, 1000))
+                       np.linspace(min(active)-1, max(active)+1, 1000))
 
     R = pearson(surface, active, estimator=est)
     s_std = (est(surface**2) - est(surface)**2)**0.5
@@ -231,14 +240,15 @@ while True:
         if not requirement[n]:
             ax.plot(surface[n], active[n], '.', color='red')
 
-    ax.plot(s_m, a_m, '+', ms=5, color=[1,0,1])
-    ax.text(s_m, a_m, f"({round(s_m, 2)},{round(a_m, 2)})", color=[1,1,0], fontsize=8)
-    levs = np.array([1e-4, 2e-4, 5e-4, 0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1])*max(Z.flatten())
+    ax.plot(s_m, a_m, '+', ms=5, color=[1, 0, 1])
+    ax.text(s_m, a_m, f"({round(s_m, 2)},{round(a_m, 2)})", color=[1, 1, 0],
+            fontsize=8)
+    levs = np.array([1e-4, 2e-4, 5e-4, 0.001, 0.002, 0.005, 0.01,
+                     0.02, 0.05, 0.1, 0.2, 0.5, 1])*max(Z.flatten())
     cs = ax.contour(X, Y, Z, linewidths=0.7, levels=levs)
     ax.clabel(cs, inline=True, fontsize=8)
 
-
-    ## %%
+    # # %%
     # Only those which meet the requirement of Z >= 0.05
     new_req = []
     for n in range(len(requirement)):
@@ -253,7 +263,7 @@ while True:
         print("No trimming")
         break
     ix += 1
-# setting number of curves 
+# setting number of curves
 # if initial:
 #     solutions += [sol]
 #     config[material]['average'][direct] = sol
@@ -272,67 +282,79 @@ r = dif(sol)
 fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(10, 10))
 ax[0].set_title(f"Average data with stddev")
 ax[1].set_title("Residual with average and deviations")
-ax[0].plot(x_av, y, '.', ms=0.7, lw=0.7, color=glob_style([0,0,0]))
-ax[0].plot(x_av, y_stdev, '.', ms=0.7, lw=0.7, color=glob_style([0,0,1]))
+ax[0].plot(x_av, y, '.', ms=0.7, lw=0.7, color=glob_style([0, 0, 0]))
+ax[0].plot(x_av, y_stdev, '.', ms=0.7, lw=0.7, color=glob_style([0, 0, 1]))
 
 ax1 = plt.twinx(ax[0])
 # ax1.set_ylim([0, 1200])
 
-ax[1].plot(x_av, r, '.', lw=0.7, ms=0.8, color=glob_style([0,0,0]))
+ax[1].plot(x_av, r, '.', lw=0.7, ms=0.8, color=glob_style([0, 0, 0]))
 
 ax2 = plt.twinx(ax[1])
 ax2.set_ylim([0, 1200])
-ax2.plot(x_av, gdev(r, K), '-', lw=0.7, ms=0.7, color=glob_style([0,0,0]))
-ax2.plot(x_av, np.linspace(np.std(r), np.std(r), len(x_av)), '--', lw=0.7, ms=0.7, color=glob_style([0,0,1]))
+ax2.plot(x_av, gdev(r, K), '-', lw=0.7, ms=0.7, color=glob_style([0, 0, 0]))
+ax2.plot(x_av, np.linspace(np.std(r), np.std(r), len(x_av)), '--', lw=0.7,
+         ms=0.7, color=glob_style([0, 0, 1]))
 
-ax[1].plot(x_av, np.linspace(np.mean(r), np.mean(r), len(x_av)), '-.', lw=0.7, ms=0.7, color=glob_style([0,0,1]))
-ax[1].plot(x_av, smoothing(r, K), '--', lw=0.7, ms=0.7, color=glob_style([0,0,0]))
+ax[1].plot(x_av, np.linspace(np.mean(r), np.mean(r), len(x_av)), '-.', lw=0.7,
+           ms=0.7, color=glob_style([0, 0, 1]))
+ax[1].plot(x_av, smoothing(r, K), '--', lw=0.7, ms=0.7,
+           color=glob_style([0, 0, 0]))
 
-ax1.plot(x_av, gd, '-', lw=0.7, ms=0.7, color=glob_style([0,0,0]))
+ax1.plot(x_av, gd, '-', lw=0.7, ms=0.7, color=glob_style([0, 0, 0]))
 
-ax[0].plot(x_av, lorentz(v, x_av), '-', color=glob_style([0,0,0,0.3]), lw=0.9, ms=0.9)
-ax[0].plot(x_av, raman(sol, x_av), '-', color=glob_style([1,0,0]), lw=0.9)
-ax[0].plot(x_av, raman(sol, x_av)+smoothing(r, K), '--', color=glob_style([1,0,0]), lw=0.9)
+ax[0].plot(x_av, lorentz(v, x_av), '-', color=glob_style([0, 0, 0, 0.3]),
+           lw=0.9, ms=0.9)
+ax[0].plot(x_av, raman(sol, x_av), '-', color=glob_style([1, 0, 0]),
+           lw=0.9)
+ax[0].plot(x_av, raman(sol, x_av)+smoothing(r, K), '--',
+           color=glob_style([1, 0, 0]), lw=0.9)
 
 # fig.savefig(f"./.data/{direct}/fit/average_{direct}_fit.pdf")
-## %%
+# # %%
 # Visual of spectrum components
 fig, ax = plt.subplots(figsize=(10, 10))
 ax.set_title(f"Spectral components for average model")
 ax.set_xlim([min(x_av), max(x_av)])
 ax.set_ylim([0, max(y)*1.25])
-ax.plot(x_av, y, '.', ms=2, color=glob_style([0,0,0]))
-ax.plot(x_av, raman(sol, x_av), '-', lw=0.7, color=glob_style([1,0,0]))
+ax.plot(x_av, y, '.', ms=2, color=glob_style([0, 0, 0]))
+ax.plot(x_av, raman(sol, x_av), '-', lw=0.7, color=glob_style([1, 0, 0]))
 for n in range(0, len(sol)-1, 3):
-    ax.plot(x_av, lorentz(sol[n:n+3], x_av), lw=0.7, color=glob_style([0,0,0,0.3]))
+    ax.plot(x_av, lorentz(sol[n:n+3], x_av), lw=0.7,
+            color=glob_style([0, 0, 0, 0.3]))
     if 500 < sol[n:n+3][2] < 3000:
         if 0 < abs(sol[n:n+3][0]) < 14000:
-            ax.plot(sol[n:n+3][2], abs(sol[n:n+3][0]), '.', ms=2, color=glob_style([1,0,0]))
-            ax.text(sol[n:n+3][2], abs(sol[n:n+3][0]), f"[{int(n/3)}]  {str(round(abs(sol[n:n+3][2]),1))}")
+            ax.plot(sol[n:n+3][2], abs(sol[n:n+3][0]), '.', ms=2,
+                    color=glob_style([1, 0, 0]))
+            ax.text(sol[n:n+3][2], abs(sol[n:n+3][0]),
+                    f"[{int(n/3)}]  {str(round(abs(sol[n:n+3][2]),1))}")
         else:
-            ax.plot(sol[n:n+3][2], abs(sol[n:n+3][0]), '.', ms=2, color=glob_style([1,0,0]))
-            ax.text(sol[n:n+3][2], 14000, f"[{int(n/3)}]  {str(round(abs(sol[n:n+3][2]),1))}")
+            ax.plot(sol[n:n+3][2], abs(sol[n:n+3][0]), '.', ms=2,
+                    color=glob_style([1, 0, 0]))
+            ax.text(sol[n:n+3][2], 14000,
+                    f"[{int(n/3)}]  {str(round(abs(sol[n:n+3][2]),1))}")
 # fig.savefig(f"./.data/{direct}/comp/average_{direct}_comp.pdf")
 
 # %%
 # Proper logic
 for dset in tbl.keys():
-    ## %%
+    # # %%
     # Attempt to apply model to data set
     # fig, ax = plt.subplots(nrows=2, figsize=(10, 10))
     # dset = '#0' its the same '000'
     y = np.array(tbl[dset]['#Intensity'])
-    sol, h = leastsq(residual(raman, x_av, y), config[material]['average'][direct])
+    sol, h = leastsq(residual(raman, x_av, y),
+                     config[material]['average'][direct])
     dif = residual(raman, x_av, y)
     print(f"For {dset} deg.: {dif(sol).dot(dif(sol))}")
 
     ix = 0
-    ## %%
+    # # %%
     # Main resgd fitter
     M = config[material]['M']['init']
     while len(sol)/3 < config[material]['init']:
         gd = (gdev(r, M)**2 + gav(r, M)**2)  # V2
-        # resgd = lambda s: residual(raman, x_av, y, 1/gd)(s)*np.array(s).dot(np.array(s))
+        # resgd = resg_gen(raman, x_av, y, 1/gd)
         resgd = residual(raman, x_av, y, 1/gd)
         p = xpeak(x_av, gd, max(gd), max(gd)/2)
         hmhw = abs(x_av[p[0]] - x_av[p[-1]])/2
@@ -350,7 +372,7 @@ for dset in tbl.keys():
     M = config[material]['M']['final']
     while len(sol)/3 < config[material]['count']:
         gd = (gdev(r, M)**2 + gav(r, M)**2)  # V2
-        # resgd = lambda s: residual(raman, x_av, y, 1/gd)(s)*np.array(s).dot(np.array(s))
+        # resgd = resg_gen(raman, x_av, y, 1/gd)
         resgd = residual(raman, x_av, y, 1/gd)
         p = xpeak(x_av, gd, max(gd), max(gd)/2)
         hmhw = abs(x_av[p[0]] - x_av[p[-1]])/2
@@ -365,10 +387,9 @@ for dset in tbl.keys():
         # print(f"{int(len(sol)/3)}: {r.dot(r)}")
         print(f"{int(len(sol)/3)}: {percentage(r.dot(r), y.dot(y))}")
 
-
-        ## %%
+        # # %%
         # Active vs overall surface
-    
+
         # fig, ax = plt.subplots()
         # fig.set_size_inches(15, 15)
         # ax.set_title(f"Curve selector for {dset} degrees {direct}")
@@ -382,7 +403,7 @@ for dset in tbl.keys():
             # ax.text(surface[-1], active[-1], f"{int(n/3)}", ha='left')
         surface = np.array(surface)
         active = np.array(active)
-        # ax.plot(surface, active, '.', color=glob_style([0,0,0]))
+        # ax.plot(surface, active, '.', color=glob_style([0, 0, 0]))
         # ax.set_aspect(aspect='equal', adjustable='box')
 
         def bivariate(u, v, r):
@@ -397,11 +418,12 @@ for dset in tbl.keys():
                 x_std = np.linspace(x_std, x_std, len(x_p))
             if '__len__' not in dir(y_std):
                 y_std = np.linspace(y_std, y_std, len(y_p))
+
             def DENS(u, v):
                 data = 0
                 for n in range(len(x_p)):
-                    data += single_density((u-x_p[n])/x_std[n], 
-                                        (v-y_p[n])/y_std[n])/(x_std[n]*y_std[n])
+                    data += (single_density((u-x_p[n])/x_std[n],
+                             (v-y_p[n])/y_std[n])/(x_std[n]*y_std[n]))
                 return np.array(data)
             return DENS
 
@@ -422,25 +444,27 @@ for dset in tbl.keys():
         s_m = est(surface)
         a_m = est(active)
 
-        X, Y = np.meshgrid(np.linspace(min(surface)-1, max(surface)+1, 1000), 
+        X, Y = np.meshgrid(np.linspace(min(surface)-1, max(surface)+1, 1000),
                            np.linspace(min(active)-1, max(active)+1, 1000))
 
         Z = bivariate((X-s_m)/s_std, (Y-a_m)/a_std, R)
 
-        requirement = bivariate((surface-s_m)/s_std, (active-a_m)/a_std, R) >= 0.05
+        requirement = bivariate((surface-s_m)/s_std,
+                                (active-a_m)/a_std, R) >= 0.05
 
         # for n in range(len(surface)):
         #     if not requirement[n]:
         #         ax.plot(surface[n], active[n], '.', color='red')
 
-        # ax.plot(s_m, a_m, '+', ms=5, color=[1,0,1])
-        # ax.text(s_m, a_m, f"({round(s_m, 2)},{round(a_m, 2)})", color=[1,1,0], fontsize=8)
-        # levs = np.array([1e-4, 2e-4, 5e-4, 0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1])*max(Z.flatten())
+        # ax.plot(s_m, a_m, '+', ms=5, color=[1, 0, 1])
+        # ax.text(s_m, a_m, f"({round(s_m, 2)},{round(a_m, 2)})",
+        #         color=[1, 1, 0], fontsize=8)
+        # levs = np.array([1e-4, 2e-4, 5e-4, 0.001, 0.002, 0.005, 0.01, 0.02,
+        #                  0.05, 0.1, 0.2, 0.5, 1])*max(Z.flatten())
         # cs = ax.contour(X, Y, Z, linewidths=0.7, levels=levs)
         # ax.clabel(cs, inline=True, fontsize=8)
 
-
-        ## %%
+        # # %%
         # Only those which meet the requirement of Z >= 0.05
         new_req = []
         for n in range(len(requirement)):
@@ -455,7 +479,7 @@ for dset in tbl.keys():
             print("No trimming")
             break
         ix += 1
-    # setting number of curves 
+    # setting number of curves
     # if initial:
     #     solutions += [sol]
     #     config[material]['average'][direct] = sol
@@ -463,7 +487,7 @@ for dset in tbl.keys():
     #     initial = False
     config[material]['solutions'][direct].append(sol)
 
-    ## %%
+    # # %%
     # Main fitter plotter
     K = M
     res = residual(raman, x_av, y, y_stdev)
@@ -471,46 +495,59 @@ for dset in tbl.keys():
     fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(10, 10))
     ax[0].set_title(f"Data with stddev for {dset} degrees {direct}")
     ax[1].set_title("Residual with average and deviations")
-    ax[0].plot(x_av, y, '.', ms=0.7, lw=0.7, color=glob_style([0,0,0]))
-    ax[0].plot(x_av, y_stdev, '.', ms=0.7, lw=0.7, color=glob_style([0,0,1]))
+    ax[0].plot(x_av, y, '.', ms=0.7, lw=0.7, color=glob_style([0, 0, 0]))
+    ax[0].plot(x_av, y_stdev, '.', ms=0.7, lw=0.7, color=glob_style([0, 0, 1]))
 
     ax1 = plt.twinx(ax[0])
     ax1.set_ylim([0, 1200])
 
-    ax[1].plot(x_av, r, '.', lw=0.7, ms=0.8, color=glob_style([0,0,0]))
+    ax[1].plot(x_av, r, '.', lw=0.7, ms=0.8, color=glob_style([0, 0, 0]))
 
     ax2 = plt.twinx(ax[1])
     ax2.set_ylim([0, 1200])
-    ax2.plot(x_av, gdev(r, K), '-', lw=0.7, ms=0.7, color=glob_style([0,0,0]))
-    ax2.plot(x_av, np.linspace(np.std(r), np.std(r), len(x_av)), '--', lw=0.7, ms=0.7, color=glob_style([0,0,1]))
+    ax2.plot(x_av, gdev(r, K), '-', lw=0.7, ms=0.7,
+             color=glob_style([0, 0, 0]))
+    ax2.plot(x_av, np.linspace(np.std(r), np.std(r), len(x_av)), '--', lw=0.7,
+             ms=0.7, color=glob_style([0, 0, 1]))
 
-    ax[1].plot(x_av, np.linspace(np.mean(r), np.mean(r), len(x_av)), '-.', lw=0.7, ms=0.7, color=glob_style([0,0,1]))
-    ax[1].plot(x_av, smoothing(r, K), '--', lw=0.7, ms=0.7, color=glob_style([0,0,0]))
+    ax[1].plot(x_av, np.linspace(np.mean(r), np.mean(r), len(x_av)), '-.',
+               lw=0.7, ms=0.7, color=glob_style([0, 0, 1]))
+    ax[1].plot(x_av, smoothing(r, K), '--', lw=0.7, ms=0.7,
+               color=glob_style([0, 0, 0]))
 
-    ax1.plot(x_av, gdev(r, K), '-', lw=0.7, ms=0.7, color=glob_style([0,0,0]))
+    ax1.plot(x_av, gdev(r, K), '-', lw=0.7, ms=0.7,
+             color=glob_style([0, 0, 0]))
 
-    ax[0].plot(x_av, lorentz(v, x_av), '-', color=glob_style([0,0,0,0.3]), lw=0.9, ms=0.9)
-    ax[0].plot(x_av, raman(sol, x_av), '-', color=glob_style([1,0,0]), lw=0.9)
-    ax[0].plot(x_av, raman(sol, x_av)+smoothing(r, K), '--', color=glob_style([1,0,0]), lw=0.9)
+    ax[0].plot(x_av, lorentz(v, x_av), '-', color=glob_style([0, 0, 0, 0.3]),
+               lw=0.9, ms=0.9)
+    ax[0].plot(x_av, raman(sol, x_av), '-', color=glob_style([1, 0, 0]),
+               lw=0.9)
+    ax[0].plot(x_av, raman(sol, x_av)+smoothing(r, K), '--',
+               color=glob_style([1, 0, 0]), lw=0.9)
 
     fig.savefig(f"./.data/{direct}/fit/{dset}_{direct}_fit.pdf")
-    ## %%
+    # # %%
     # Visual of spectrum components
     fig, ax = plt.subplots(figsize=(10, 10))
     ax.set_title(f"Spectral components for {dset} degrees {direct}")
     ax.set_xlim([min(x_av), max(x_av)])
     ax.set_ylim([0, max(y)*1.25])
-    ax.plot(x_av, y, '.', ms=2, color=glob_style([0,0,0]))
-    ax.plot(x_av, raman(sol, x_av), '-', lw=0.7, color=glob_style([1,0,0]))
+    ax.plot(x_av, y, '.', ms=2, color=glob_style([0, 0, 0]))
+    ax.plot(x_av, raman(sol, x_av), '-', lw=0.7, color=glob_style([1, 0, 0]))
     for n in range(0, len(sol)-1, 3):
-        ax.plot(x_av, lorentz(sol[n:n+3], x_av), lw=0.7, color=glob_style([0,0,0,0.3]))
+        ax.plot(x_av, lorentz(sol[n:n+3], x_av), lw=0.7,
+                color=glob_style([0, 0, 0, 0.3]))
         if 500 < sol[n:n+3][2] < 3000:
             if 0 < abs(sol[n:n+3][0]) < 14000:
-                ax.plot(sol[n:n+3][2], abs(sol[n:n+3][0]), '.', ms=2, color=glob_style([1,0,0]))
-                ax.text(sol[n:n+3][2], abs(sol[n:n+3][0]), f"[{int(n/3)}]  {str(round(abs(sol[n:n+3][2]),1))}")
+                ax.plot(sol[n:n+3][2], abs(sol[n:n+3][0]), '.', ms=2,
+                        color=glob_style([1, 0, 0]))
+                ax.text(sol[n:n+3][2], abs(sol[n:n+3][0]),
+                        f"[{int(n/3)}]  {str(round(abs(sol[n:n+3][2]),1))}")
             else:
-                ax.plot(sol[n:n+3][2], abs(sol[n:n+3][0]), '.', ms=2, color=glob_style([1,0,0]))
-                ax.text(sol[n:n+3][2], 14000, f"[{int(n/3)}]  {str(round(abs(sol[n:n+3][2]),1))}")
+                ax.plot(sol[n:n+3][2], abs(sol[n:n+3][0]), '.', ms=2,
+                        color=glob_style([1, 0, 0]))
+                ax.text(sol[n:n+3][2], 14000,
+                        f"[{int(n/3)}]  {str(round(abs(sol[n:n+3][2]),1))}")
     # fig.savefig(f"./.data/{direct}/comp/{dset}_{direct}_comp.pdf")
     plt.show()
 # %%
@@ -553,10 +590,10 @@ for nr, s in enumerate(sols):
         hmhw = abs(s[1+3*ix])
         # amp = abs(s[0+3*ix])*hmhw
         amp = abs(s[0+3*ix])
-        ax2.plot(nr*10, pos, '.', color=[0,1,1], ms=5)
-        ax3.plot(nr*10, hmhw, '.', color=[0,1,1], ms=5)
-        ax4.plot(np.pi*nr/18, amp, '.', color=[0,1,1], ms=5)
-        
+        ax2.plot(nr*10, pos, '.', color=[0, 1, 1], ms=5)
+        ax3.plot(nr*10, hmhw, '.', color=[0, 1, 1], ms=5)
+        ax4.plot(np.pi*nr/18, amp, '.', color=[0, 1, 1], ms=5)
+
         # ax1.text(s[2+3*ix], lorentz(s[3*ix:3*ix+3], s[2+3*ix]), f"{nr}/{ix}")
         if amp > max_y:
             max_y = amp*1.1
@@ -568,10 +605,11 @@ ax4.set_ylim([0, max_y])
 fig = pex.scatter()
 fig.layout.template = 'plotly_dark'
 gd = (gdev(r, M)**2 + gav(r, M)**2)  # V2
+mat_av_data = config[material]['average'][direct]
 traces = [
     {
         'x': x_av,
-        'y': residual(raman, x_av, y_av, 1/gd)(config[material]['average'][direct]),
+        'y': residual(raman, x_av, y_av, 1/gd)(mat_av_data),
         'name': 'Res1 (penalty 1/gd)',
         'mode': 'markers',
         'marker': {'size': 2},
@@ -579,7 +617,7 @@ traces = [
     },
     {
         'x': x_av,
-        'y': residual(raman, x_av, y_av, gd)(config[material]['average'][direct]),
+        'y': residual(raman, x_av, y_av, gd)(mat_av_data),
         'name': 'Res2 (penalty gd)',
         'mode': 'markers',
         'marker': {'size': 2},
